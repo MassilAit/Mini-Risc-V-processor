864b0d8173cac3f37b5d0383480d78266b8f35e1
Massil <massilaitabdeslam@gmail.com>
2024-11-17 23:08:21 -0500

    Decode missing I-type

--- sources/pipeline/riscv_instruction_decode/decode.vhd
+++ sources/pipeline/riscv_instruction_decode/decode.vhd
@@ -67,6 +67,116 @@ begin
         case( opcode ) is
         
             when "0110011" =>  -- R-Type
+                o_rs1_addr <= i_instr(19 downto 15); -- using rs1
+                o_rs2_addr <= i_instr(24 downto 20); -- using rs2
+
+                o_rd_addr <= i_instr(11 downto 7); --output register
+
+                o_imm <= (others => '0'); -- No immediate value
+
+                if funct3="000" and funct7="0000000" then --ADD
+                    -- ALU  used : rs1 + rs2
+                    o_arith <= '0';  -- addition
+                    o_sign <= '0';   -- Programmer
+                    o_opcode  <= ALUOP_ADD;
+                    o_shamt <= (others => '0');
+
+                    -- Flags
+                    o_wb <= '1';   -- Write to register   
+
+                
+                elsif funct3="000" and funct7="0100000" then --SUB
+                    -- ALU  used : rs1 - rs2
+                    o_arith <= '1';  -- substraction
+                    o_sign <= '0';   -- Programmer
+                    o_opcode  <= ALUOP_ADD;
+                    o_shamt <= (others => '0');
+
+                    -- Flags
+                    o_wb <= '1';   -- Write to register   
+
+
+                elsif funct3="001" and funct7="0000000" then --SLL
+                    -- ALU  used : rs1 << rs2
+                    o_arith <= '0';  
+                    o_sign <= '0';   
+                    o_opcode  <= ALUOP_SL;
+                    o_shamt <= (others => '0');
+
+                    -- Flags
+                    o_wb <= '1';   -- Write to register 
+
+                elsif funct3="010" and funct7="0000000" then --SLT
+                    -- ALU  used : rs1 < rs2 (signed)
+                    o_arith <= '0';  
+                    o_sign <= '1';   
+                    o_opcode  <= ALUOP_SLT;
+                    o_shamt <= (others => '0');
+
+                    -- Flags
+                    o_wb <= '1';   -- Write to register 
+
+                elsif funct3="011" and funct7="0000000" then --SLTU
+                    -- ALU  used : rs1 < rs2 (unsigned)
+                    o_arith <= '0';  
+                    o_sign <= '0';   
+                    o_opcode  <= ALUOP_SLT;
+                    o_shamt <= (others => '0');
+
+                    -- Flags
+                    o_wb <= '1';   -- Write to register 
+
+                elsif funct3="100" and funct7="0000000" then --XOR
+                    -- ALU  used : rs1 xor rs2 
+                    o_arith <= '0';  
+                    o_sign <= '0';   
+                    o_opcode  <= ALUOP_XOR;
+                    o_shamt <= (others => '0');
+
+                    -- Flags
+                    o_wb <= '1';   -- Write to register 
+
+                elsif funct3="101" and funct7="0000000" then --SRL
+                    -- ALU  used : rs1 >> rs2 (logical)
+                    o_arith <= '0';  
+                    o_sign <= '0';   
+                    o_opcode  <= ALUOP_SR;
+                    o_shamt <= (others => '0');
+
+                    -- Flags
+                    o_wb <= '1';   -- Write to register 
+
+                elsif funct3="101" and funct7="0100000" then --SRA
+                    -- ALU  used : rs1 >> rs2 (arithmetic)
+                    o_arith <= '1';  
+                    o_sign <= '0';   
+                    o_opcode  <= ALUOP_SR;
+                    o_shamt <= (others => '0');
+
+                    -- Flags
+                    o_wb <= '1';   -- Write to register 
+
+                elsif funct3="110" and funct7="0000000" then --OR
+                    -- ALU  used : rs1 or rs2 
+                    o_arith <= '0';  
+                    o_sign <= '0';   
+                    o_opcode  <= ALUOP_OR;
+                    o_shamt <= (others => '0');
+
+                    -- Flags
+                    o_wb <= '1';   -- Write to register 
+
+                elsif funct3="111" and funct7="0000000" then --AND
+                    -- ALU  used : rs1 or rs2 
+                    o_arith <= '0';  
+                    o_sign <= '0';   
+                    o_opcode  <= ALUOP_AND;
+                    o_shamt <= (others => '0');
+
+                    -- Flags
+                    o_wb <= '1';   -- Write to register 
+                
+                end if ;
 
 
             when "0110111" =>  -- U-Type
